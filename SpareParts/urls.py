fb86e756b5875e9f42ab7e391d6f30b70d424d27
dawnbergdev <mmos7239@gmail.com>
2021-12-02 08:21:42 +0200

    fix login && fix logo && spareParts done

--- SpareParts/urls.py
+++ SpareParts/urls.py
@@ -4,8 +4,10 @@ from .views import *
 app_name = 'SpareParts'
 urlpatterns = [
    path('sparepartsList/', SparePartsTypeList.as_view(), name="SpareTypeList" ),
+   path('sparepartsTrachList/', SparePartsTypeTrachList.as_view(), name="SpareTypeTrachList" ),
    path('sparepartsType/create/', SparePartsTypeCreate.as_view(), name='SparePartsTypeCreate'),
    path('sparepartsType/<int:pk>/update/', SparePartsTypeUpdate.as_view(), name='SparePartsTypeUpdate'),
    path('sparepartsType/<int:pk>/delete/', SparePartsTypeDelete.as_view(), name='SparePartsTypeDelete'),
+   path('sparepartsType/<int:pk>/restore/', SparePartsTypeRestore.as_view(), name='SparePartsTypeRestore'),
 
 ]
