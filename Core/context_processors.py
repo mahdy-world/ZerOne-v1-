1ae8f1d06bf1e5d99b891f939969e7070b11c321
egy <mmos7239@gmail.com>
2022-01-02 15:50:52 +0200

    work

--- Core/context_processors.py
+++ Core/context_processors.py
@@ -3,6 +3,8 @@ from django.http import request
 from Core.models import SystemInformation
 from SpareParts.models import *
 from Machines.models import * 
+from django.utils import timezone as tz
+
 
 def allcontext(request):
     info = SystemInformation.objects.filter(id=1)
@@ -11,12 +13,15 @@ def allcontext(request):
     spare_order = SparePartsOrders.objects.filter(deleted=False)
     machine_order = MachinesOrders.objects.filter(deleted=False)
     notfaiy = MachineNotifecation.objects.filter(created_at=datetime.date.today())
+    
     context = {
         'info':info,
         'spare_parts':spare_parts,
         'machines':machines,
         'spare_order':spare_order,
         'machine_order':machine_order,
-        'notifay':notfaiy
+        'notifay':notfaiy,
+        'today': datetime.date.today()
+
     }
     return context
