8d3c2f85b9ec28c00e5d7013c180a1d5afe407bb
dawnbergdev <mmos7239@gmail.com>
2022-01-19 02:31:41 +0200

    installed djnago dubeg

--- ZerOne/urls.py
+++ ZerOne/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('spare_parts/',include('SpareParts.urls')),
     path('machines/',include('Machines.urls')),
     path('treasury/',include('Treasury.urls'), ),
+    path('__debug__/', include('debug_toolbar.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
